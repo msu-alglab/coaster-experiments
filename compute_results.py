d88e6330b59d2c4eb3c91264aae57407e2e0c47b
Lucy Williams <lgw2@uw.edu>
2021-04-15 01:09:23 -0600

    use combos to iterate through

--- compute_results.py
+++ compute_results.py
@@ -33,15 +33,14 @@ if args.fd_heur:
 
 print("Computing accuracies for each of:")
 for exp_type in exp_types:
-    for length in lengths:
-        for sps in subpaths:
-            print("{},{},{}...".format(exp_type, length, sps))
-            subprocess.run([
-                "python", "process_output.py",
-                "acyclic_sc_graph_instances/len{}dem1subpaths{}/truth/graphs.truth".
-                format(length, sps),
-                "acyclic_sc_graph_instances/len{}dem1subpaths{}/predicted_{}/all_pred.txt".
-                format(length, sps, exp_type)], check=True)
+    for length, sps in combos:
+        print("{},{},{}...".format(exp_type, length, sps))
+        subprocess.run([
+            "python", "process_output.py",
+            "acyclic_sc_graph_instances/len{}dem1subpaths{}/truth/graphs.truth".
+            format(length, sps),
+            "acyclic_sc_graph_instances/len{}dem1subpaths{}/predicted_{}/all_pred.txt".
+            format(length, sps, exp_type)], check=True)
 
 def print_np_array_as_latex_table(a):
     print("\\\\\n".join([" & ".join(map(str, line)) for line in a]))
