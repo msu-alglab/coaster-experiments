e2c9c730e6db1491237ba57c25d08826fc53b86e
Lucy Williams <lgw2@uw.edu>
2021-04-15 05:19:47 -0600

    update bash files and add new ones for 4,4 only

--- run_fpt.bash
+++ run_fpt.bash
@@ -1,56 +1,79 @@
 #!/bin/bash  
 
-rm -rf run_fpt_err
-rm -rf run_fpt_out
-rm -rf run_fpt_slurm
+rm -rf run_fpt_err/
+rm -rf run_fpt_out/
+rm -rf run_fpt_slurms/
 mkdir run_fpt_err
 mkdir run_fpt_out
 mkdir run_fpt_slurms
     
-for len in 3 4; do
-    echo $len
-    for sps in 1 2 3 4; do
-        echo $sps
-        
-echo '#!/bin/bash
-
-#SBATCH --job-name    '$len'_'$sps'run_fpt_data        # job name
-#SBATCH --output      run_fpt_out/'$len'_'$sps'.out # standard output file (%j = jobid)
-#SBATCH --error       run_fpt_err/'$len'_'$sps'.err # standard error file
-#SBATCH --partition   unsafe      # queue partition to run the job in
-#SBATCH --nodes       1            # number of nodes to allocate
-#SBATCH --ntasks-per-node 1        # number of descrete tasks - keep at one except for MPI 
-#SBATCH --cpus-per-task=1          # number of CPU cores to allocate
-#SBATCH --mem         5000         # 2000 MB of Memory allocated; set --mem with care
-#SBATCH --time        0:30:00     # Maximum job run time
-##SBATCH --mail-user   $email      # user to send emails to
-##SBATCH --mail-type   ALL         # Email on: BEGIN, END, FAIL & REQUEUE
-
-python ../coaster/coaster.py acyclic_sc_graph_instances/len'$len'dem1subpaths'$sps'/graphs/sc0.graph --timeout 30' > run_fpt_slurms/$len_$sps.slurm
-sbatch run_fpt_slurms/$len_$sps.slurm
-done
-done
+#for len in 3 4; do
+#    echo $len
+#    for sps in 1 2 3 4; do
+#        rm -rf "acyclic_sc_graph_instances/len${len}dem1subpaths${sps}/predicted_fpt/"
+#        mkdir "acyclic_sc_graph_instances/len${len}dem1subpaths${sps}/predicted_fpt/"
+#        echo $sps
+#        path="acyclic_sc_graph_instances/len${len}dem1subpaths${sps}/graphs/"
+#        echo $path
+#        numfiles=$(ls -l $path | wc -l)
+#        numfiles=$(($numfiles - 2))
+#        # numfiles=1
+#
+#        for p in $(seq 0 $numfiles); do
+#            filename=$path'sc'$p'.graph'
+#            echo $filename
+#        
+#echo '#!/bin/bash
+# 
+##SBATCH --job-name    '$len'_'$sps'_'$p'run_fpt_data        # job name
+##SBATCH --output      run_fpt_out/'$len'_'$sps'_'$p'.out # standard output file (%j = jobid)
+##SBATCH --error       run_fpt_err/'$len'_'$sps'_'$p'.err # standard error file
+##SBATCH --partition   unsafe      # queue partition to run the job in
+##SBATCH --nodes       1            # number of nodes to allocate
+##SBATCH --ntasks-per-node 1        # number of descrete tasks - keep at one except for MPI 
+##SBATCH --cpus-per-task=1          # number of CPU cores to allocate
+##SBATCH --mem         12000         # 2000 MB of Memory allocated; set --mem with care
+##SBATCH --time        3:00:00     # Maximum job run time
+###SBATCH --mail-user   $email      # user to send emails to
+###SBATCH --mail-type   ALL         # Email on: BEGIN, END, FAIL & REQUEUE
+# 
+#python ../coaster/coaster.py '$filename' --timeout 30' > run_fpt_slurms/${len}_${sps}_${p}.slurm
+#sbatch run_fpt_slurms/${len}_${sps}_${p}.slurm
+#done
+#done
+#done
 
 for len in 1; do
     echo $len
     for sps in 0; do
         echo $sps
+        rm -rf "acyclic_sc_graph_instances/len${len}dem1subpaths${sps}/predicted_fpt/"
+        mkdir "acyclic_sc_graph_instances/len${len}dem1subpaths${sps}/predicted_fpt/"
+        path="acyclic_sc_graph_instances/len${len}dem1subpaths${sps}/graphs/"
+        echo $path
+        numfiles=$(ls -l $path | wc -l)
+        numfiles=$(($numfiles - 2))
+
+        for p in $(seq 0 $numfiles); do
+            filename=$path'sc'$p'.graph'
+            echo $filename
         
 echo '#!/bin/bash
 
-#SBATCH --job-name    '$len'_'$sps'run_fpt_data        # job name
-#SBATCH --output      run_fpt_out/'$len'_'$sps'.out # standard output file (%j = jobid)
-#SBATCH --error       run_fpt_err/'$len'_'$sps'.err # standard error file
+#SBATCH --job-name    '$len'_'$sps'_'$p'run_fpt_data        # job name
+#SBATCH --output      run_fpt_out/'$len'_'$sps'_'$p'.out # standard output file (%j = jobid)
+#SBATCH --error       run_fpt_err/'$len'_'$sps'_'$p'.err # standard error file
 #SBATCH --partition   unsafe      # queue partition to run the job in
 #SBATCH --nodes       1            # number of nodes to allocate
 #SBATCH --ntasks-per-node 1        # number of descrete tasks - keep at one except for MPI 
 #SBATCH --cpus-per-task=1          # number of CPU cores to allocate
-#SBATCH --mem         5000         # 2000 MB of Memory allocated; set --mem with care
+#SBATCH --mem         20000         # 2000 MB of Memory allocated; set --mem with care
 #SBATCH --time        0:30:00     # Maximum job run time
 ##SBATCH --mail-user   $email      # user to send emails to
 ##SBATCH --mail-type   ALL         # Email on: BEGIN, END, FAIL & REQUEUE
 
-python ../coaster/coaster.py acyclic_sc_graph_instances/len'$len'dem1subpaths'$sps'/graphs/sc0.graph --timeout 30' > run_fpt_slurms/$len_$sps.slurm
-sbatch run_fpt_slurms/$len_$sps.slurm
+python ../coaster/coaster.py '$filename' --timeout 30' > run_fpt_slurms/${len}_${sps}_${p}.slurm
+sbatch run_fpt_slurms/${len}_${sps}_${p}.slurm
+done
 done
 done
