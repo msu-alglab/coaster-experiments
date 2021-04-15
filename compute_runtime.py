07dafde8b3ecc46017c8dbcf8fff56850f734f16
Lucy Williams <lgw2@uw.edu>
2021-04-15 13:48:03 -0600

    add script to compute runtime

--- compute_runtime.py
+++ compute_runtime.py
@@ -12,23 +12,40 @@ parser.add_argument('--fd_heur', default=False, action='store_true')
 args = parser.parse_args()
     
 # start all_ouputs file with k values
-Path("runtime_outputs.txt").unlink()
-f = open("all_outputs.txt", "a")
+f = open("runtime_outputs.txt", "w")
 f.write("2,3,4,5,6,7,8,9,10\n")
 f.close()
 
-def get_file_runtimes(f, total_counts, min_, max_):
-    pass
+# get instances info
+instance_counts = defaultdict(int)
+with open("instance_counts.txt") as f:
+    for line in f:
+        key = line.split()[0].strip()
+        value = int(line.split()[1].strip())
+        instance_counts[key] = value
+
+def get_file_runtimes(f, total, counts, min_, max_):
+    rf = open(f, "r")
+    for line in rf:
+        graphname = line.split()[0].split(".graph")[0]
+        if instance_counts[graphname] == 18:
+            runtime = float(line.strip().split()[1])
+            total += runtime
+            counts += 1
+            min_ = min(min_, runtime)
+            max_ = max(max_, runtime)
+    return total, counts, min_, max_
 
 def get_all_exp_runtimes(dirname):
     runtimes_dir = Path(dirname)
     filenames = list(filter(Path.is_file, runtimes_dir.glob('**/*')))
-    total = defaultdict(int)
-    counts = defaultdict(int)
-    min_ = defaultdict(float)
-    max = defaultdict(float)
+    total = 0
+    counts = 0
+    min_ = 100
+    max_ = 0
     for f in filenames:
-        get_file_runtimes(f, total, counts, min_, max_)
+        total, counts, min_, max_ = get_file_runtimes(f, total, counts, min_, max_)
+    print("count", counts, "avg", total/counts, "min", min_, "max", max_)
         
 # the lengths and subpaths we want to run experiments for
 # (we will also create no subpath versions)
@@ -44,16 +61,11 @@ if args.fd_heur:
 
 print("Computing runtimes for each of:")
 for exp_type in exp_types:
-    if exp_type == "fpt":
-        dirname = "run_fpt"
-    elif exp_name == "fd_heur":
-        dirname = "run_heuristic"
-    dirname += "_out"
     for length, sps in combos:
         print("{},{},{}...".format(exp_type, length, sps))
         runtimes_dir = "acyclic_sc_graph_instances/len{}dem1subpaths{}/runtimes_{}/".\
     format(length, sps, exp_type)
-        get_all_exp_runtimes(runtimes_dirname)
+        get_all_exp_runtimes(runtimes_dir)
 
 def print_np_array_as_latex_table(a):
     print("\\\\\n".join([" & ".join(map(str, line)) for line in a]))
