6d1dc0678ff8164acf857f6274a2b30cc173a1ae
Lucy Williams <lgw2@uw.edu>
2021-11-29 08:54:27 -0700

    Remove unneeded scripts

--- run_for_mem.bash
+++
@@ -1,5 +0,0 @@
-for INDEX in {1..31}
-do
-    /usr/bin/time -v python ~/coaster/coaster.py acyclic_sc_graph_instances/len4dem1subpaths4/graphs/sc0.graph --indices $INDEX --timeout 30 > std_out_files/out$INDEX.txt 2> std_error_files/error$INDEX.txt
-done
-
